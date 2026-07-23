aa13aa7999c5e5d462d8d16ffeb8c91f6b40dcc6
amit negi <amitnegi@gmail.com>
2026-07-23 12:01:25 +0530

    add token based auth for dagshub

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -12,9 +12,22 @@ import dagshub
 import os
 
 
+# Set up DagsHub credentials for MLflow tracking
+# its key-based authentication...
 
-mlflow.set_tracking_uri('https://dagshub.com/amitnegionway/mini-mlflow-temp.mlflow')
-dagshub.init(repo_owner='amitnegionway', repo_name='mini-mlflow-temp', mlflow=True)
+dagshub_token=os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"]=dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"]=dagshub_token
+
+dagshub_url="https://dagshub.com"
+repo_owner="amitnegionway"
+repo_name="mini-mlflow-temp"
+
+# set up mlfloe tracking URI
+mlflow.set_tracking_uri(f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow")
 
 # logging configuration
 logger = logging.getLogger('model_evaluation')
