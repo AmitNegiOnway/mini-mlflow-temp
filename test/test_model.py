1541c62a9e8b8dc0176044668fb8f0d3e244e421
amit negi <amitnegi@gmail.com>
2026-07-23 16:59:24 +0530

    add test5

--- test/test_model.py
+++ test/test_model.py
@@ -38,14 +38,14 @@ class TestModelLoading(unittest.TestCase):
         # Load holdout test data
         cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
 
-    # @staticmethod
-    # def get_latest_model_version(model_name, stage="Staging"):
-    #     client = mlflow.MlflowClient()
-    #     latest_version = client.get_latest_versions(model_name, stages=[stage])
-    #     return latest_version[0].version if latest_version else None
+    @staticmethod
+    def get_latest_model_version(model_name, stage="Staging"):
+        client = mlflow.MlflowClient()
+        latest_version = client.get_latest_versions(model_name, stages=[stage])
+        return latest_version[0].version if latest_version else None
 
-    # def test_model_loaded_properly(self):
-    #     self.assertIsNotNone(self.new_model)
+    def test_model_loaded_properly(self):
+        self.assertIsNotNone(self.new_model)
 
 
     # def test_model_signature(self):
