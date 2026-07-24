930c5cf55aa1981b9a9949ea73ee0dd99d05af66
amit negi <amitnegi@gmail.com>
2026-07-24 12:30:18 +0530

    add little changes 7 in  promoted_model.py

--- script/promoted_model.py
+++ script/promoted_model.py
@@ -2,42 +2,50 @@ import os
 import mlflow
 
 def promoted_model():
-    # Set up DagsHub credentials for MLflow tracking
-    dagshub_token=os.getenv("DAGSHUB_PAT")
+    # Set up DagsHub credentials
+    dagshub_token = os.getenv("DAGSHUB_PAT")
     if not dagshub_token:
         raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-    os.environ["MLFLOW_TRACKING_USERNAME"]=dagshub_token
-    os.environ["MLFLOW_TRACKING_PASSWORD"]=dagshub_token
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
     dagshub_url = "https://dagshub.com"
     repo_owner = "amitnegionway"
     repo_name = "mini-mlflow-temp"
 
-    # Set up MLflow tracking URI
-    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+    mlflow.set_tracking_uri(
+        f"{dagshub_url}/{repo_owner}/{repo_name}.mlflow"
+    )
 
-    client=mlflow.MlflowClient()
+    client = mlflow.MlflowClient()
 
-    model_name="my_model"
+    model_name = "my_model"
 
-    # get the latest version in staging 
+    # Get staging model
+    latest_version_staging = client.get_model_version_by_alias(
+        model_name,
+        alias="staging"
+    ).version
 
-    latest_version_staging=client.get_model_version_by_alias(model_name,alias='staging')[0].version
+    # Get current champion (optional)
+    try:
+        champ_version = client.get_model_version_by_alias(
+            model_name,
+            alias="champion"
+        )
+        print(f"Current champion: Version {champ_version.version}")
+    except Exception:
+        print("No champion model found.")
 
-    # archive the current production model
-    champ_version=client.get_model_version_by_alias(model_name,alias='champion')
-
-    if champ_version:
-      print(f"Current champion: Version {champ_version.version}")
-
-    # promted the new model to champion 
+    # Promote staging -> champion
     client.set_registered_model_alias(
         name=model_name,
-        version=latest_version_staging,
-        stage="champion"
+        alias="champion",
+        version=latest_version_staging
     )
-    print(f"Model version {latest_version_staging} promoted to Production")
+
+    print(f"Model version {latest_version_staging} promoted to champion")
 
 if __name__ == "__main__":
     promoted_model()
